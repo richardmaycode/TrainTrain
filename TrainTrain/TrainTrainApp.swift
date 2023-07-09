48ebb9695ff87b36932cc7ad149e3143f131b55c
Richard Wise <richard.wise@hey.com>
2023-07-08 21:04:32 -0400

    Initial Commit

---
+++ TrainTrain/TrainTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainTrainApp.swift
+//  TrainTrain
+//
+//  Created by Richard Wise on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct TrainTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
